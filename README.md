b820fe871104a5e0b2d2b572b9474a8ba62605bc
Girma Nigusse <xgirma@gmail.com>
2019-10-28 19:09:53 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # selenium-webdriver (commonjs, jest)
 selenium-webdriver end-to-end test automation boilerplate project. Using commonjs &amp; jest.
+
+## Getting Started
+
+    git clone git@github.com:e2e-boilerplates/selenium-webdriver-commonjs-jest.git
+    cd selenium-webdriver-commonjs-jest
+    npm install 
+    npm run test
---
+++ __tests__/search.spec.js
@@ -0,0 +1,37 @@
+const { Builder, By, Key, until } = require("selenium-webdriver");
+require("chromedriver");
+
+describe("google Search", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await new Builder().forBrowser("chrome").build();
+    browser.get("https://www.google.com");
+  });
+
+  afterAll(() => {
+    browser.quit();
+  });
+
+  test("should be on google search page", async () => {
+    const searchBox = await browser.findElement(By.name("q"));
+    await browser.wait(until.elementIsVisible(searchBox), 5000);
+
+    const title = await browser.getTitle();
+    expect(title).toEqual("Google");
+  });
+
+  test("should search for Cheese!", async () => {
+    const searchBox = await browser.findElement(By.name("q"));
+    expect(await searchBox.isDisplayed()).toBe(true);
+    searchBox.sendKeys("Cheese!", Key.ENTER);
+  });
+
+  test('the page title should start with "Cheese!"', async () => {
+    await browser.wait(until.urlContains("search"), 5000);
+
+    const title = await browser.getTitle();
+    const isTitleStartWithCheese = title.lastIndexOf("Cheese!", 0) === 0;
+    expect(isTitleStartWithCheese).toBe(true);
+  });
+});
