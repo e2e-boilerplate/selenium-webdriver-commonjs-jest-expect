7c1b8f735dfdcdd172f80b69d65af4130daf4750
Girma Nigusse <xgirma@gmail.com>
2020-01-25 16:30:39 -0500

    update tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # selenium-webdriver (commonjs, jest)
 
-[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-commonjs-jest](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jest/workflows/NodeCI/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jest/actions?workflow=NodeCI)
+[![GitHub Actions status | e2e-boilerplates/selenium-webdriver-commonjs-jest](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jest/workflows/selenium-webdriver-commonjs-jest/badge.svg)](https://github.com/e2e-boilerplates/selenium-webdriver-commonjs-jest/actions?workflow=selenium-webdriver-commonjs-jest)
 
 selenium-webdriver end-to-end test automation boilerplate project. Using commonjs &amp; jest.
 
---
+++ __tests__/sandbox.spec.js
@@ -0,0 +1,33 @@
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+require("chromedriver");
+
+const options = new chrome.Options();
+const chromeOptions = process.env.GITHUB_ACTIONS ? options.headless() : options;
+
+describe("Sandbox", () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(chromeOptions)
+      .build();
+    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+  });
+
+  afterAll(() => {
+    browser.quit();
+  });
+
+  test("should be on Sandbox", async () => {
+    const title = await browser.getTitle();
+    const header = await browser.findElement(By.css("h1"));
+
+    expect(title).toEqual("Sandbox");
+    header.getText().then(text => {
+      expect(text).toEqual("Sandbox");
+    });
+  });
+});
