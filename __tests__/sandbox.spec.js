ba67b12daead4532e1e9d33bea4b2b58b1c071a6
Girma Nigusse <xgirma@gmail.com>
2020-03-08 23:37:45 -0400

    update,update test url

--- __tests__/sandbox.spec.js
+++ __tests__/sandbox.spec.js
@@ -14,7 +14,7 @@ describe("Sandbox", () => {
       .forBrowser("chrome")
       .setChromeOptions(chromeOptions)
       .build();
-    browser.get("https://e2e-boilerplates.github.io/sandbox/");
+    browser.get("https://e2e-boilerplate.github.io/sandbox/");
   });
 
   afterAll(() => {
